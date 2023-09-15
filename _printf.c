594b56684c700ba9b87af1d3601248e7f935a96b
Triad01 <luckypee01@gmail.com>
2023-09-15 15:56:20 +0100

    Rough implementation of printf to handle %s, %c, and %

---
+++ _printf.c
@@ -0,0 +1,67 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+/**
+* _printf - custom printf() to print formatted strings to stdout
+* @format: format string to be printed 
+* Return: 0 if successfull and -1 on error
+*/
+int _printf(const char *format, ...)
+{
+	int num_of_characters, _str_length;
+	char character;
+	char *string;
+	va_list list_of_args;
+	
+	num_of_characters = _str_length = 0;
+
+	if (format == NULL)
+		return (-1);
+	va_start(list_of_args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			num_of_characters++;
+		}
+		else
+		{
+			format++;
+		}
+		if (*format == '\0')
+			break;
+		if (*format == '%')
+		{
+			write(1, format,1);
+			num_of_characters++;
+		}
+		else if(*format == 'c')
+		{
+			character = va_arg(list_of_args, int);
+			write(1, &character, 1);
+			num_of_characters++;
+		}
+		else if (*format == 's')
+		{
+			string = va_arg(list_of_args, char*);
+			while (string[_str_length] != '\0')
+				_str_length++;
+			write(1, string, _str_length);
+			num_of_characters=+ _str_length;
+		}
+		format++;
+	}
+	va_end(list_of_args);
+	return (num_of_characters);
+}
+void main(void)
+{
+	_printf("Triad\n");
+	_printf("%c\n", 'v');
+	_printf("%s\n", "String");
+	_printf("%%\n");
+	_printf("Hello %s\n", "Triad");
+}
