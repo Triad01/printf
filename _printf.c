9c5ff4f0a4ec85d0e654f75cc7632372e4b0c5fd
Triad01 <luckypee01@gmail.com>
2023-09-16 15:49:36 +0100

    implemented specifiers for integers

--- _printf.c
+++ _printf.c
@@ -2,6 +2,8 @@
 #include <stdarg.h>
 #include <stddef.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 /**
 * _printf - custom printf() to print formatted strings to stdout
 * @format: format string to be printed
@@ -9,14 +11,15 @@
 */
 int _printf(const char *format, ...)
 {
-	int num_of_characters, _str_length;
+	int num_of_characters, _str_length, number;
 	va_list list_of_args;
+	char num_str[20];
 
 	num_of_characters = _str_length = 0;
+
 	if (format == NULL)
 		return (-1);
 	va_start(list_of_args, format);
-
 	while (*format)
 	{
 		if (*format != '%')
@@ -43,6 +46,13 @@ int _printf(const char *format, ...)
 			{
 				_str_length = string_handler(va_arg(list_of_args, char*));
 				num_of_characters += _str_length;
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				number = va_arg(list_of_args, int);
+				snprintf(num_str, sizeof(num_str), "%d", number);
+				write(1, num_str, strlen(num_str));
+				num_of_characters += strlen(num_str);
 			}
 		}
 		format++;
