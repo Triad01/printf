86eea359fa9277af784fca9314984fae956fad9e
Triad01 <luckypee01@gmail.com>
2023-09-18 07:46:38 +0100

    removed standard libraries from header file

--- main.h
+++ main.h
@@ -1,8 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
-#include <string.h>
-#include <unistd.h>
 int _printf(const char *format, ...);
 char character_handler(char);
 int string_handler(char *);
