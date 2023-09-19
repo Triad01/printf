528f2a6c53466ae0557021a4c1367008316ee26e
Triad01 <luckypee01@gmail.com>
2023-09-19 11:46:53 +0100

    modified code

--- _printf.c
+++ _printf.c
@@ -1,30 +1,26 @@
 #include "main.h"
 #include <stdarg.h>
-#include <stddef.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 
 /**
  * _printf - custom printf() to print formatted strings to stdout
- * description:a function that produces output according to a format.
  * @format: format string to be printed
- * Return: 0 if successful and -1 on error
+ * @...: variable number of arguments to be formatted and printed
+ * Return: the number of characters printed
  */
-
 int _printf(const char *format, ...);
 int _printf(const char *format, ...)
 {
-	int num_of_characters, _str_length;
-	int number;
+	int num_of_characters = 0;
 	va_list list_of_args;
-	char num_str[20], character;
-
-	num_of_characters = 0;
 
 	if (format == NULL)
 		return (-1);
+
 	va_start(list_of_args, format);
+
 	while (*format)
 	{
 		if (*format != '%')
@@ -37,55 +33,54 @@ int _printf(const char *format, ...)
 			format++;
 			if (*format == '\0')
 				break;
-			if (*format == '%')
-			{
-				write(1, format, 1);
-				num_of_characters++;
-			}
-			else if (*format == 'c')
-			{
-				character = va_arg(list_of_args, int);
-				write(1, &character, 1);
-				num_of_characters++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(list_of_args, char*);
 
-				if (str == NULL)
-				{
-					write(1, "(null)", 6);
-					num_of_characters += 6;
-				}
-				else
-				{
-					_str_length = 0;
-
-					while (str[_str_length] != '\0')
+			switch (*format)
+			{
+				case 'c':
 					{
-						write(1, &str[_str_length], 1);
-						_str_length++;
-					}
-					num_of_characters += _str_length;
-				}
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				number = va_arg(list_of_args, int);
-				snprintf(num_str, sizeof(num_str), "%d", number);
-				write(1, num_str, strlen(num_str));
-				num_of_characters += strlen(num_str);
-			}
-			else
-			{
-				char err_msg[7];
-				int err_msg_len = snprintf(err_msg, sizeof(err_msg), "%%%c", *format);
+						char character = va_arg(list_of_args, int);
 
-				write(1, err_msg, err_msg_len);
-				num_of_characters += err_msg_len;
+						write(1, &character, 1);
+						num_of_characters++;
+						break;
+					}
+				case 's':
+					{
+						char *str = va_arg(list_of_args, char *);
+
+						if (str == NULL)
+							  str = "(null)";
+						write(1, str, strlen(str));
+						num_of_characters += strlen(str);
+						break;
+					}
+				case 'd':
+				case 'i':
+					{
+						int number = va_arg(list_of_args, int);
+						char num_str[20];
+						int len = snprintf(num_str, sizeof(num_str), "%d", number);
+
+						write(1, num_str, len);
+						num_of_characters += len;
+						break;
+					}
+				case '%':
+					{
+						write(1, "%", 1);
+						num_of_characters++;
+						break;
+					}
+				default:
+					{
+						write(1, format - 1, 2);
+						num_of_characters += 2;
+						break;
+					}
 			}
 		}
 		format++;
-	} va_end(list_of_args);
+	}
+	va_end(list_of_args);
 	return (num_of_characters);
 }
