816c5f711fd831ff93b06a3bfaf3d3eabaa6b3a6
Triad01 <luckypee01@gmail.com>
2023-09-19 12:01:00 +0100

    ad

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "main.h"
-
+#include <unistd.h>
 /**
  * _printf - custom printf() to print formatted strings to stdout
  * @format: format string to be printed
@@ -62,6 +62,14 @@ int _printf(const char *format, ...)
 					printf("(null)");
 					printed_chars += 6;
 				}
+			}
+			else
+			{
+				char err_msg[7];
+				int err_msg_len = snprintf(err_msg, sizeof(err_msg), "%%%c", *format);
+
+				write(1, err_msg, err_msg_len);
+				printed_chars += err_msg_len;
 			}
 		}
 		format++;
